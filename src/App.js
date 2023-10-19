7695c668c91902700923dd4033c4a7aad5ed2727
Gustavo Madeira Krieger <gustavo.m.krieger@gmail.com>
2023-10-19 15:48:44 -0300

    board working

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import { useState } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 
-export default function PlayRandomMoveEngine() {
+export default function App() {
   const [game, setGame] = useState(new Chess());
 
   function onDrop(sourceSquare, targetSquare) {
-    const move = game.move({
-      from: sourceSquare,
-      to: targetSquare,
-      promotion: "q", // always promote to a queen for example simplicity
-    });
+    try{
+      game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q", // always promote to a queen for example simplicity
+      });
+    }catch{// illegal move
+      return false;
+    }
+    let gameCopy = new Chess(game.fen())
+    setGame(gameCopy);
 
-    setGame(game);
-
-    // illegal move
-    if (move === null) return false;
     return true;
   }
 
@@ -26,14 +28,14 @@ export default function PlayRandomMoveEngine() {
     <main>
         <div class="container my-2">
             <div class="row align-items-center">
-                <div class="col-md-8">
-                    <h1 class="text-align-center">Chess Dapp</h1>
-                    <div class="row my-3 text-align-center">
-                        <div class="col-md-12">
-                          <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+                <div class="center">
+                    <h1>Chess Dapp</h1>
+                    <div>
+                        <div id="MyBoard" class="ex1">
+                          <Chessboard position={game.fen()} onPieceDrop={onDrop} onMouseOverSquare={onMouseoverSquare}/>
                         </div>
                         <div class="col-md-12">
-                            <h2>Status</h2>
+                            <h2 class="center">Status</h2>
                             <p><span id="status">No check, checkmate, or draw.</span></p>
                         </div>
                     </div>
