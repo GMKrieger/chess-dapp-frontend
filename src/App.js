538ea09f117a20bd5843ef257526e14a75d51539
Gustavo Madeira Krieger <gustavo.m.krieger@gmail.com>
2023-10-19 19:41:58 -0300

    Added status update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,37 @@ import { Chessboard } from "react-chessboard";
 export default function App() {
   const [game, setGame] = useState(new Chess());
 
+  const [status, setStatus] = useState(<span>No check, checkmate, or draw.</span>)
+
+  function sayHello() {
+    alert('You clicked me!');
+  }
+  // Usage
+  <button onClick={sayHello}>Default</button>;
+
+  function updateStatus() {
+    const color = game.turn() === 'w' ? 'White' : 'Black'
+
+    if (game.isCheckmate()) {
+      setStatus(<span><b>Checkmate!</b> <b>{color}</b> lost.</span>);
+    } else if (game.isInsufficientMaterial()) {
+      setStatus(<span>It's a <b>draw!</b> (Insufficient Material)</span>);
+    } else if (game.isThreefoldRepetition()) {
+      setStatus(<span>It's a <b>draw!</b> (Threefold Repetition)</span>);
+    } else if (game.isStalemate()) {
+      setStatus(<span>It's a <b>draw!</b> (Stalemate)</span>);
+    } else if (game.isDraw()) {
+      setStatus(<span>It's a <b>draw!</b> (50-move Rule)</span>);
+    } else if (game.isCheck()) {
+      setStatus(<span><b>{color}</b> is in <b>check!</b></span>);
+      return false;
+    } else {
+      setStatus(<span>No check, checkmate, or draw.</span>);
+      return false;
+    }
+    return true;
+  }
+
   function onDrop(sourceSquare, targetSquare) {
     try{
       game.move({
@@ -17,28 +48,35 @@ export default function App() {
     }catch{// illegal move
       return false;
     }
+    updateStatus();
+
     let gameCopy = new Chess(game.fen())
     setGame(gameCopy);
 
     return true;
   }
 
-  return (    
+  return (
   <body>
     <main>
-        <div class="container my-2">
-            <div class="row align-items-center">
+        <div>
+            <div>
+                <div class="center-left">
+                  <button class="start" onClick={sayHello}>
+                    Start new game
+                  </button>
+                </div>
                 <div class="center">
+                  <div>
                     <h1>Chess Dapp</h1>
-                    <div>
-                        <div id="MyBoard" class="ex1">
-                          <Chessboard position={game.fen()} onPieceDrop={onDrop}/>
-                        </div>
-                        <div class="col-md-12">
-                            <h2 class="center">Status</h2>
-                            <p><span id="status">No check, checkmate, or draw.</span></p>
-                        </div>
-                    </div>
+                  </div>
+                  <div id="MyBoard" class="ex1">
+                    <Chessboard position={game.fen()} onPieceDrop={onDrop}/>
+                  </div>
+                  <div>
+                      <h2>Status</h2>
+                      <p>{status}</p>
+                  </div>
                 </div>
             </div>
         </div>
